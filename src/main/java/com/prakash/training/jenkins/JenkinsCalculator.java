f2f01875b15648985c7950f19ef9ff8e600af1ad
Prakash Jadhav <53575105+prakashsjadhav@users.noreply.github.com>
2021-07-16 18:52:17 +0530

    Update JenkinsCalculator.java

--- src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
+++ src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
@@ -2,10 +2,11 @@ package com.prakash.training.jenkins;
 
 public class JenkinsCalculator {
 	public int addNumbers(int numberOne, int numberTwo) {
-		System.out.print("Inside addNumbers"); 
+		System.out.println("Inside addNumbers"); 
 	  return numberOne + numberTwo;
 	}
 	public int subtractNumbers(int numberOne, int numberTwo) {
+		System.out.println("Inside subtractNumbers");
 		  return numberOne - numberTwo;
 		}
 		
