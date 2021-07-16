d0bdb232b388df1c779fe67b6c033c2a86659710
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2021-07-16 18:44:26 +0530

    statement added in addNumbers function

--- src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
+++ src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
@@ -2,6 +2,7 @@ package com.prakash.training.jenkins;
 
 public class JenkinsCalculator {
 	public int addNumbers(int numberOne, int numberTwo) {
+		System.out.print("Adding numbers"); 
 	  return numberOne + numberTwo;
 	}
 	public int subtractNumbers(int numberOne, int numberTwo) {
