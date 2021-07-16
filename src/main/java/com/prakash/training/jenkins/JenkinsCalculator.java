6d1057a46d55b90a057df898a07caa6981d5b55a
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2021-07-16 18:40:11 +0530

    Adding files in MavenJob

---
+++ src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,12 @@
+package com.prakash.training.jenkins;
+
+public class JenkinsCalculator {
+	public int addNumbers(int numberOne, int numberTwo) {
+	  return numberOne + numberTwo;
+	}
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		  return numberOne - numberTwo;
+		}
+		
+
+}
