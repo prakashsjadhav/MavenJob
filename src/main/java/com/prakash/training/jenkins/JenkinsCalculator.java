2b9cb93d94cfa42eb45a7c32ca02fc5593d2d86a
Prakash Jadhav <53575105+prakashsjadhav@users.noreply.github.com>
2021-07-17 19:42:27 +0530

    Update JenkinsCalculator.java

--- src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
+++ src/main/java/com/prakash/training/jenkins/JenkinsCalculator.java
@@ -6,7 +6,7 @@ public class JenkinsCalculator {
 	  return numberOne + numberTwo;
 	}
 	public int subtractNumbers(int numberOne, int numberTwo) {
-		System.out.println("Subtracting numbers");
+		System.out.println("Subtracting numbers with - operat");
 		  return numberOne - numberTwo;
 		}
 		
